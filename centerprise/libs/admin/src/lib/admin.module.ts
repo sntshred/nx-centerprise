b4b3af5abc8f921031515d12a63254db5cd33ae2
Santosh Chavan <santosh.aryan9@gmail.com>
2022-01-25 23:06:12 -0500

    added dashboard component

--- centerprise/libs/admin/src/lib/admin.module.ts
+++ centerprise/libs/admin/src/lib/admin.module.ts
@@ -2,12 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin.component';
 import { RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild([{ path: '', component: AdminComponent }]),
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    LayoutModule,
   ],
-  declarations: [AdminComponent],
+  declarations: [AdminComponent, DashboardComponent],
 })
 export class AdminModule {}
